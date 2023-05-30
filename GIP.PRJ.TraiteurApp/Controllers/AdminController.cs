95798734ee748cf00708f9f8396b90945fb663b9
Mslbdmd <99437765+Mslbdmd@users.noreply.github.com>
2023-05-30 09:44:21 +0200

    new controller

--- GIP.PRJ.TraiteurApp/Controllers/AdminController.cs
+++ GIP.PRJ.TraiteurApp/Controllers/AdminController.cs
@@ -6,62 +6,26 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GIP.PRJ.TraiteurApp.Models;
-using GIP.PRJ.TraiteurApp.Services.Interfaces;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.Identity.Client;
-using GIP.PRJ.TraiteurApp.Services;
-using Kendo.Mvc.UI;
-using Kendo.Mvc.Extensions;
 using GIP.PRJ.TraiteurApp.ViewModels.Admin;
 
 namespace GIP.PRJ.TraiteurApp.Controllers
 {
     public class AdminController : Controller
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly IRolesService _rolesService;
         private readonly TraiteurAppDbContext _context;
 
-        public AdminController(TraiteurAppDbContext context, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IRolesService rolesService)
+        public AdminController(TraiteurAppDbContext context)
         {
             _context = context;
-            _userManager = userManager;
-            _roleManager = roleManager;
-            _rolesService = rolesService;
         }
 
-        // GET: CreateRolesViewModels
+        // GET: Admin
         public async Task<IActionResult> Index()
         {
-            /*var traiteurAppDbContext = _context.CreateRolesViewModel.Include(c => c.IdentityUser);
-            return View(await traiteurAppDbContext.ToListAsync());*/
-            var role = (from r in _context.Roles where r.Name.Contains("Administrator") select r).FirstOrDefaultAsync();
-            var admins = await _context.Users.ToListAsync();
-
-            var adminVM = admins.Select(user => new UserViewModel
-            {
-                Email = user.Email,
-                RoleName = "Administrator"
-
-            }).ToList();
-
-            var role2 = (from r in _context.Roles where r.Name.Contains("Cook") select r).FirstOrDefaultAsync();
-            var cooks = await _context.Users.ToListAsync();
-
-            var cookVM = cooks.Select(cook => new UserViewModel
-            {
-                Email = cook.Email,
-                RoleName = "Cook"
-
-            }).ToList();
-
-            var model = new GroupedViewModel { Admins = adminVM, Users = cookVM };
-            return View(model);
+              return View(await _context.CreateRolesViewModel.ToListAsync());
         }
 
-        // GET: CreateRolesViewModels/Details/5
+        // GET: Admin/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.CreateRolesViewModel == null)
@@ -69,48 +33,39 @@ namespace GIP.PRJ.TraiteurApp.Controllers
                 return NotFound();
             }
 
-            var createRolesViewModel = await _context.CreateRolesViewModel
+            var userViewModel = await _context.CreateRolesViewModel
                 .FirstOrDefaultAsync(m => m.Id == id);
-
-            if (createRolesViewModel == null)
+            if (userViewModel == null)
             {
                 return NotFound();
             }
 
-            return View(createRolesViewModel);
+            return View(userViewModel);
         }
 
-        // GET: CreateRolesViewModels/Create
+        // GET: Admin/Create
         public IActionResult Create()
         {
-            UserViewModel vm = new UserViewModel();
-            vm.Roles = _context.Roles.ToList();
-            return View(vm);
+            return View();
         }
 
-        // POST: CreateRolesViewModels/Create
+        // POST: Admin/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Email,Password,RoleId")] UserViewModel vm)
+        public async Task<IActionResult> Create([Bind("Id,Email,RoleName")] UserViewModel userViewModel)
         {
             if (ModelState.IsValid)
             {
-                var newUser = new IdentityUser(vm.Email);
-                IdentityResult identityResult = await _userManager.CreateAsync(newUser, "Password");
-                if (identityResult.Succeeded)
-                {
-                    await _userManager.AddToRoleAsync(newUser, vm.RoleName);
-                }
-
+                _context.Add(userViewModel);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            vm.Roles = _context.Roles.ToList();
-            return View(vm);
+            return View(userViewModel);
         }
 
-        // GET: CreateRolesViewModels/Edit/5
+        // GET: Admin/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.CreateRolesViewModel == null)
@@ -118,23 +73,22 @@ namespace GIP.PRJ.TraiteurApp.Controllers
                 return NotFound();
             }
 
-            var createRolesViewModel = await _context.CreateRolesViewModel.FindAsync(id);
-            if (createRolesViewModel == null)
+            var userViewModel = await _context.CreateRolesViewModel.FindAsync(id);
+            if (userViewModel == null)
             {
                 return NotFound();
             }
-            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id");
-            return View(createRolesViewModel);
+            return View(userViewModel);
         }
 
-        // POST: CreateRolesViewModels/Edit/5
+        // POST: Admin/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,IdentityUserId,Roles")] UserViewModel createRolesViewModel)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Email,RoleName")] UserViewModel userViewModel)
         {
-            if (id != createRolesViewModel.Id)
+            if (id != userViewModel.Id)
             {
                 return NotFound();
             }
@@ -143,12 +97,12 @@ namespace GIP.PRJ.TraiteurApp.Controllers
             {
                 try
                 {
-                    _context.Update(createRolesViewModel);
+                    _context.Update(userViewModel);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!CreateRolesViewModelExists(createRolesViewModel.Id))
+                    if (!UserViewModelExists(userViewModel.Id))
                     {
                         return NotFound();
                     }
@@ -159,11 +113,10 @@ namespace GIP.PRJ.TraiteurApp.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id");
-            return View(createRolesViewModel);
+            return View(userViewModel);
         }
 
-        // GET: CreateRolesViewModels/Delete/5
+        // GET: Admin/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.CreateRolesViewModel == null)
@@ -171,18 +124,17 @@ namespace GIP.PRJ.TraiteurApp.Controllers
                 return NotFound();
             }
 
-            var createRolesViewModel = await _context.CreateRolesViewModel
+            var userViewModel = await _context.CreateRolesViewModel
                 .FirstOrDefaultAsync(m => m.Id == id);
-
-            if (createRolesViewModel == null)
+            if (userViewModel == null)
             {
                 return NotFound();
             }
 
-            return View(createRolesViewModel);
+            return View(userViewModel);
         }
 
-        // POST: CreateRolesViewModels/Delete/5
+        // POST: Admin/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -191,17 +143,17 @@ namespace GIP.PRJ.TraiteurApp.Controllers
             {
                 return Problem("Entity set 'TraiteurAppDbContext.CreateRolesViewModel'  is null.");
             }
-            var createRolesViewModel = await _context.CreateRolesViewModel.FindAsync(id);
-            if (createRolesViewModel != null)
+            var userViewModel = await _context.CreateRolesViewModel.FindAsync(id);
+            if (userViewModel != null)
             {
-                _context.CreateRolesViewModel.Remove(createRolesViewModel);
+                _context.CreateRolesViewModel.Remove(userViewModel);
             }
             
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CreateRolesViewModelExists(int id)
+        private bool UserViewModelExists(int id)
         {
           return _context.CreateRolesViewModel.Any(e => e.Id == id);
         }
