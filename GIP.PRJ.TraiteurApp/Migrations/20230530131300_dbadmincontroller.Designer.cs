29e24d3a184d3b9bdd7c59e2ae90a4302893c073
Mslbdmd <99437765+Mslbdmd@users.noreply.github.com>
2023-05-30 15:16:16 +0200

    migration added

--- GIP.PRJ.TraiteurApp/Migrations/20230530131300_dbadmincontroller.Designer.cs
+++ GIP.PRJ.TraiteurApp/Migrations/20230530131300_dbadmincontroller.Designer.cs
@@ -4,6 +4,7 @@ using GIP.PRJ.TraiteurApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GIP.PRJ.TraiteurApp.Migrations
 {
     [DbContext(typeof(TraiteurAppDbContext))]
-    partial class TraiteurAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230530131300_dbadmincontroller")]
+    partial class dbadmincontroller
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
