064629574799071b39bf9218769a4d846f6c1b8f
Mslbdmd <99437765+Mslbdmd@users.noreply.github.com>
2023-06-01 15:36:12 +0200

    changes - data anotations 4 models

--- GIP.PRJ.TraiteurApp/Models/MenuItem.cs
+++ GIP.PRJ.TraiteurApp/Models/MenuItem.cs
@@ -6,25 +6,21 @@ namespace GIP.PRJ.TraiteurApp.Models
     {
         public int Id { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Naam")]
         [Required(ErrorMessage = "Naam is verplicht.")]
         public string Name { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Omschrijving")]
         [Required(ErrorMessage = "Omschrijving is verplicht.")]
         public string Description { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "Prijs")]
         [Range(0, double.MaxValue, ErrorMessage = "Prijs mag niet negatief zijn.")]
         public decimal Price { get; set; }
 
-        [Display(Name = "")]
+        [Display(Name = "MenuItemCategorieId")]
         public int MenuItemCategoryId { get; set; }
-
-        [Display(Name = "")]
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
-
-        [Display(Name = "")]
         public virtual MenuItemCategory MenuItemCategory { get; set; }
     }
 }
