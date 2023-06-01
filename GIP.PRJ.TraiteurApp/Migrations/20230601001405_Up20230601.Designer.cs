38be51333df0289be54107e6bbfff3cb23c73009
Bingshen Chen <cbs2315453671@gmail.com>
2023-06-01 02:17:19 +0200

    Databaseupdata: add-migration

--- GIP.PRJ.TraiteurApp/Migrations/20230601001405_Up20230601.Designer.cs
+++ GIP.PRJ.TraiteurApp/Migrations/20230601001405_Up20230601.Designer.cs
@@ -4,6 +4,7 @@ using GIP.PRJ.TraiteurApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GIP.PRJ.TraiteurApp.Migrations
 {
     [DbContext(typeof(TraiteurAppDbContext))]
-    partial class TraiteurAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230601001405_Up20230601")]
+    partial class Up20230601
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
