305e051c89cc4600d233a8f7d8c801ec5c2d7d8b
Mslbdmd <99437765+Mslbdmd@users.noreply.github.com>
2023-06-03 15:24:11 +0200

    added customer repository

---
+++ GIP.PRJ.TraiteurApp/Repository/CustomerRepository.cs
@@ -0,0 +1,63 @@
+﻿using GIP.PRJ.TraiteurApp.Models;
+using GIP.PRJ.TraiteurApp.Repository.Interface;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using Telerik.SvgIcons;
+
+namespace GIP.PRJ.TraiteurApp.Repository
+{
+    public class CustomerRepository : ICustomerRespository
+    {
+        private readonly TraiteurAppDbContext _dbContext;
+
+        public CustomerRepository(TraiteurAppDbContext dbContext)
+        {     
+            _dbContext = dbContext;
+        }
+        public void CreateCustomer(Customer c)
+        {
+            _dbContext.Customers.Add(c);
+        }
+
+        public void DeleteCustomer(int id)
+        {
+            Customer customer = _dbContext.Customers.Find(id);
+            _dbContext.Customers.Remove(customer);
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            return _dbContext.Customers.Find(id);
+        }
+
+        public Customer GetCustomerByIdentityAsync(string id)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _dbContext.Customers.ToList();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateCustomer(Customer c)
+        {
+            _dbContext.Entry(c).State = EntityState.Modified;
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GIP.PRJ.TraiteurApp/Repository/Interface/ICustomerRespository.cs
@@ -0,0 +1,17 @@
+﻿using GIP.PRJ.TraiteurApp.Models;
+
+namespace GIP.PRJ.TraiteurApp.Repository.Interface
+{
+    public interface ICustomerRespository
+    {
+        IEnumerable<Customer> GetCustomers();
+        Customer GetCustomerById(int id);
+        Customer GetCustomerByIdentityAsync(string id);
+        void CreateCustomer(Customer c);
+        void UpdateCustomer(Customer c);
+        void DeleteCustomer(int id);
+        void Save();
+
+
+    }
+}
