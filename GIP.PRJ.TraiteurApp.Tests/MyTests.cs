846845c64123e4c64becaa47fc3a7c747887e09a
Mslbdmd <99437765+Mslbdmd@users.noreply.github.com>
2023-06-03 16:02:19 +0200

    added a Test project

---
+++ GIP.PRJ.TraiteurApp.Tests/MyTests.cs
@@ -0,0 +1,44 @@
+﻿using GIP.PRJ.TraiteurApp.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIP.PRJ.TraiteurApp.Tests
+{
+    //MIMIC OF THE DATABASE
+    [TestClass]
+    public class MyTests
+    {
+        private TraiteurAppDbContext _context;
+        [TestInitialize]
+        public void Initialize()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            var options = new DbContextOptionsBuilder<TraiteurAppDbContext>()
+            .UseSqlite(connection)
+            .Options;
+
+            _context = new TraiteurAppDbContext(options);
+            _context.Database.EnsureCreated();
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+
+
+    }
+}
---
+++ GIP.PRJ.TraiteurApp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GIP.PRJ.TraiteurApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
