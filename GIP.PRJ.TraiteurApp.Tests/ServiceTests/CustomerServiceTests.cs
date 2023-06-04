bbd04ae5f35bfb1e6c407dcf410137e8ce2c0b59
Mslbdmd <99437765+Mslbdmd@users.noreply.github.com>
2023-06-04 15:00:37 +0200

    commit servicetest

--- GIP.PRJ.TraiteurApp.Tests/ServiceTests/CustomerServiceTests.cs
+++ GIP.PRJ.TraiteurApp.Tests/ServiceTests/CustomerServiceTests.cs
@@ -1,22 +1,32 @@
-﻿using GIP.PRJ.TraiteurApp.Models;
+﻿using GIP.PRJ.TraiteurApp.Controllers;
+using GIP.PRJ.TraiteurApp.Models;
+using GIP.PRJ.TraiteurApp.Repository;
+using GIP.PRJ.TraiteurApp.Repository.Interface;
+using GIP.PRJ.TraiteurApp.Services;
 using GIP.PRJ.TraiteurApp.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Moq;
-using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Telerik.SvgIcons;
 
 namespace GIP.PRJ.TraiteurApp.Tests.ServiceTests
 {
+    [TestClass]
     public class CustomerServiceTests
     {
         private TraiteurAppDbContext _context;
+        private CustomerService _customerService;
         [TestInitialize]
-        public void Initialize()
+        public void Setup()
         {
             var connection = new SqliteConnection("DataSource=:memory:");
             connection.Open();
@@ -25,16 +35,7 @@ namespace GIP.PRJ.TraiteurApp.Tests.ServiceTests
             .Options;
             _context = new TraiteurAppDbContext(options);
             _context.Database.EnsureCreated();
-            // use _context where needed
-
-            //mocks
-            
-
-        }
-        [TestMethod]
-        public void Customer_When_Customer()
-        {
-            var mockCustomerService = new Mock<ICustomerService>();
+            _customerService = new CustomerService(_context);
         }
         [TestCleanup]
         public void Cleanup()
@@ -42,6 +43,74 @@ namespace GIP.PRJ.TraiteurApp.Tests.ServiceTests
             _context.Database.EnsureDeleted();
             _context.Dispose();
         }
+        [TestMethod]
+        public async Task CreateCustomer_Success()
+        {
+            // Arrange
+            var customer = new Customer { Id = 1, Name = "Test" };
+            // Act
+            await _customerService.CreateCustomerAsync(customer);
+            // Assert
+
+        var createdCustomer = await _context.Customers.FirstOrDefaultAsync(o =>
+        o.Id == customer.Id);
+            Assert.IsNotNull(createdCustomer);
+            // Additional assertions on the created customer properties
+        }
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public async Task CreateCustomer_InvalidData()
+        {
+            // Arrange
+            var invalidCustomer = new Customer { };
+            // Act
+            await _customerService.CreateCustomerAsync(invalidCustomer);
+            // Assert
+            // Exception is expected to be thrown
+        }
+        [TestMethod]
+        public async Task GetCustomerById_Success()
+        {
+            // Arrange
+            var customer = new Customer { Id = 1, Name = "Test" };
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+            // Act
+            var retrievedCustomer = await
+            _customerService.GetCustomerByIdAsync(customer.Id);
+            // Assert
+            Assert.IsNotNull(retrievedCustomer);
+            Assert.AreEqual(customer.Id, retrievedCustomer.Id);
+        }
+        [TestMethod]
+        public async Task GetCustomerById_CustomerNotFound()
+        {
+            // Arrange
+            var nonExistentCustomerId = 123;
+            // Act
+            var retrievedCustomer = await
+            _customerService.GetCustomerByIdAsync(nonExistentCustomerId);
+            // Assert
+            Assert.IsNull(retrievedCustomer);
+        }
+        [TestMethod]
+        public async Task UpdateCustomer_Success()
+        {
+            // Arrange
+            var customer = new Customer { Id = 1, Name = "No name??" };
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+            customer.Name = "Name updated!";
+
+            // Act
+        await _customerService.UpdateCustomerAsync(customer);
+            // Assert
+            var retrievedCustomer = await _context.Customers.FirstOrDefaultAsync(o =>
+            o.Id == customer.Id);
+            Assert.IsNotNull(retrievedCustomer);
+            Assert.AreEqual(customer.Name, "Name updated!");
+        }
+
     }
 }
 
